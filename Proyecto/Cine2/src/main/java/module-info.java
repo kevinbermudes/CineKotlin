5960abb74270bfccb61566de6208ea64286574b6
kevinbermudes <kevinber783@gmail.com>
2024-05-19 17:49:37 +0200

    Java

--- Proyecto/Cine2/src/main/java/module-info.java
+++ Proyecto/Cine2/src/main/java/module-info.java
@@ -2,6 +2,10 @@ module org.example.cine2 {
     requires javafx.controls;
     requires javafx.fxml;
     requires kotlin.stdlib;
+    requires koin.core.jvm;
+    requires logging.jvm;
+    requires kotlinx.serialization.core;
+    requires kotlin.result.jvm;
 
 
     opens org.example.cine2 to javafx.fxml;
