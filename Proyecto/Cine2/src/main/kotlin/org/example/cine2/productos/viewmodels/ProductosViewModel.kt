eef0ac7aed843a7edae2445d027c0ee9c4b2105e
Laura Garrido Arredondo <anoobmedic@gmail.com>
2024-05-20 16:01:00 +0200

    Creado ViewModel

---
+++ Proyecto/Cine2/src/main/kotlin/org/example/cine2/productos/viewmodels/ProductosViewModel.kt
@@ -0,0 +1,15 @@
+package org.example.cine2.productos.viewmodels
+
+import org.example.cine2.productos.services.database.ProductosService
+import org.example.cine2.productos.services.storage.ProductosStorage
+import org.lighthousegames.logging.logging
+
+private val logger = logging()
+class ProductosViewModel( private val service:ProductosService, private val storage:ProductosStorage) {
+   //Creamos el estado del viewmodel
+   private val state = ProductosState()
+}
+
+data class ProductosState {
+val
+}
