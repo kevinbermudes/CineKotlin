5a90eccc34866e9ea5cca2ee00f117e67e870081
Laura Garrido Arredondo <anoobmedic@gmail.com>
2024-06-09 18:59:03 +0200

    Intento de ProductosStorageImpl tests

---
+++ Proyecto/Cine2/src/test/java/services/storage/productos/ProductosStorageImplTest.kt
@@ -0,0 +1,64 @@
+package services.storage.productos
+
+import org.example.cine.productos.models.Producto
+import org.example.cine.productos.services.storage.ProductosStorageJsonImpl
+import org.junit.jupiter.api.*
+import java.io.File
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+class ProductosStorageImplTest {
+    private lateinit var productosStorageJson: ProductosStorageJsonImpl
+    private lateinit var myFile: File
+
+    @BeforeEach
+    fun setup() {
+        productosStorageJson = ProductosStorageJsonImpl()
+//        myFile = File("test.json")
+    }
+
+    @AfterEach
+    fun tearDown() {
+//        Files.deleteIfExists(myFile.toPath())
+    }
+
+    @Test
+    @Order(1)
+    fun `storeDataJson returns Ok when data is successfully written to file`() {
+        val data = listOf(
+            Producto(
+                id = 1,
+                nombre = "Producto 1",
+                precio = 10.0,
+                categoria = Producto.Categoria.BOTANA,
+                imagen = "imagen.png",
+                stock = 5
+            )
+        )
+
+        //val result = productosStorageJson.storeDataJson(myFile, data)
+
+        //assertTrue(result.isOk)
+        //assertEquals(data.size.toLong(), result.value)
+    }
+
+    @Test
+    fun `loadDataJson returns Ok when data is successfully read from file`() {
+        val data = listOf(
+            Producto(
+                id = 1,
+                nombre = "Producto 1",
+                precio = 10.0,
+                categoria = Producto.Categoria.BOTANA,
+                imagen = "imagen.png",
+                stock = 5
+            )
+        )
+
+        //  productosStorageJson.storeDataJson(myFile, data)
+
+        // val result = productosStorageJson.loadDataJson(myFile)
+
+        //assertTrue(result.isOk)
+        //assertEquals(data, result.value)
+    }
+}
